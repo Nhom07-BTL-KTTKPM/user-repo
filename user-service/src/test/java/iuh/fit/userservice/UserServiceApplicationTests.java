6c88922662fdeec51f49c8623e3d2e0a75310a9c
Nguyen Quoc Huy <122158094+Nine99Huy8181@users.noreply.github.com>
2026-05-06 14:31:42 +0700

    feat: add variable env, delete value in application

--- user-service/src/test/java/iuh/fit/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/iuh/fit/userservice/UserServiceApplicationTests.java
@@ -2,9 +2,25 @@ package iuh.fit.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
+
+    @MockitoBean
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @MockitoBean
+    private ReactiveRedisConnectionFactory reactiveRedisConnectionFactory;
+
+    @MockitoBean
+    private RabbitTemplate rabbitTemplate;
 
     @Test
     void contextLoads() {
