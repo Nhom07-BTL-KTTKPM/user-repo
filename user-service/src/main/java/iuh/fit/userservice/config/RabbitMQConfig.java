0703dac80e0090b947d4f4214b54e28d92e3d778
dgkhoa279 <vodangkhoa270904@gmail.com>
2026-05-25 07:44:33 +0700

    fix logic cart

--- user-service/src/main/java/iuh/fit/userservice/config/RabbitMQConfig.java
+++ user-service/src/main/java/iuh/fit/userservice/config/RabbitMQConfig.java
@@ -1,6 +1,9 @@
 package iuh.fit.userservice.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,11 @@ public class RabbitMQConfig {
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        var converter = new Jackson2JsonMessageConverter(new ObjectMapper().findAndRegisterModules());
+        var mapper = new DefaultJackson2JavaTypeMapper();
+        mapper.setTrustedPackages("iuh.fit.*");
+        mapper.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.INFERRED);
+        converter.setJavaTypeMapper(mapper);
+        return converter;
     }
 }
